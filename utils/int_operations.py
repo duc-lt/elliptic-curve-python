ee368ab7ce75d6eacfd078e8876fd876359b41b6
duc <ltduc169@gmail.com>
2022-10-30 23:24:24 +0700

    sửa code

--- utils/int_operations.py
+++ utils/int_operations.py
@@ -1,15 +1,7 @@
-from math import gcd, sqrt
+from math import gcd
 
 
 def is_prime(a):
-    #if a == 0 or a == 1:
-    #    return False
-    #if a == 2 or a == 3:
-    #    return True
-    #for i in range(2, int(sqrt(a)) + 1):
-    #    if a % i == 0:
-    #        return False
-    #return True
     if a < 2 or any(a % i == 0 for i in range(2, a)):
         return False
     return True
